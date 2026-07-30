48fda3c70064e7acddbf20e200144722a2ba236c
liyuier <me@yuier.com>
2026-07-31 00:32:06 +0800

    添加阻止所有提示条弹窗生成功能

--- FinishPopTip.cs
+++ FinishPopTip.cs
@@ -9,11 +9,23 @@ namespace FinishPopTip
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class FinishPopTip : BaseUnityPlugin
     {
-        private ConfigEntry<KeyCode> hotkey { get; set; }
+        public static ConfigEntry<string> BlockAllPopTips;
+        private ConfigEntry<KeyCode> hotkey;
 
         private void Awake()
         {
             hotkey = Config.Bind("General", "热键", KeyCode.S, "触发功能的热键");
+            BlockAllPopTips = Config.Bind(
+                "General",
+                "阻止所有提示条弹窗生成",
+                "否",
+                new ConfigDescription(
+                    "开启后将完全阻止提示条弹窗生成",
+                    new AcceptableValueList<string>("是", "否")
+                )
+            );
+
+            Harmony.CreateAndPatchAll(typeof(PopTipPatches));
         }
 
         private void Update()
@@ -35,6 +47,30 @@ namespace FinishPopTip
                 tips[i].TweenDestory();
                 tips.RemoveAt(i);
             }
+        }
+    }
+
+    public static class PopTipPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIPopTip), "Pop", typeof(string), typeof(PopTipIconType))]
+        public static bool Pop_Prefix()
+        {
+            return FinishPopTip.BlockAllPopTips.Value != "是";
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIPopTip), "Pop", typeof(string), typeof(string), typeof(PopTipIconType))]
+        public static bool PopWithSound_Prefix()
+        {
+            return FinishPopTip.BlockAllPopTips.Value != "是";
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIPopTip), "PopAddItem", typeof(string), typeof(int))]
+        public static bool PopAddItem_Prefix()
+        {
+            return FinishPopTip.BlockAllPopTips.Value != "是";
         }
     }
 }
