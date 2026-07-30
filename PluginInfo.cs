19cfa29277edb3430649577ecef41fe139dad28f
liyuier <me@yuier.com>
2026-07-31 00:33:10 +0800

    更新版本号

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,7 +4,7 @@
     {
         public const string PLUGIN_GUID = "com.yuier.MCS.FinishPopTip";
         public const string PLUGIN_NAME = "一键终止提示条弹窗";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
 
